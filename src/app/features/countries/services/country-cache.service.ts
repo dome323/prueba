266d551f4ca7807c84a942197c3386317670dbf3
dome323 <domeuyunkar@gmail.com>
2026-05-29 09:18:31 -0500

    prueba

--- src/app/features/countries/services/country-cache.service.ts
+++ src/app/features/countries/services/country-cache.service.ts
@@ -1,36 +1,3 @@
-import { Injectable } from '@angular/core';
-import { Country } from '../models/models.interface';
+export class CountryCacheService {
 
-@Injectable({
-  providedIn: 'root',
-})
-export class CountryCacheServices {
-
-  private readonly key = 'country-cache';
-
-  getById(id: number): Country | null {
-
-    const map = this.readMap();
-
-    return map[id] ?? null;
-  }
-
-  save(country: Country): void {
-
-    const map = this.readMap();
-
-    map[country.id] = country;
-
-    localStorage.setItem(
-      this.key,
-      JSON.stringify(map)
-    );
-  }
-
-  private readMap(): Record<number, Country> {
-
-    const raw = localStorage.getItem(this.key);
-
-    return raw ? JSON.parse(raw) : {};
-  }
 }
